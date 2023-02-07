db1b3680f223f826ef9e9b1583f705d2510e138f
Dmitriy Stepanychev <mabcroshi@gmail.com>
2023-02-07 22:10:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int nachalnSchet = 100;
+        int popolnenie = 600;
+        int bonus;
+        if (popolnenie >= 1000)
+        {
+            bonus = popolnenie / 100;
+        }
+        else bonus = 0;
+        int itogSchet = nachalnSchet + popolnenie + bonus;
+
+        System.out.println("Итого на счете: " + itogSchet);
+        System.out.println("ачислено бонусов: " + bonus);
+    }
+}
