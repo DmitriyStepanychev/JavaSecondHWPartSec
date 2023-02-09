fcbb168fd9d220a52119bb887ceff7f32f0846fb
Dmitriy Stepanychev <mabcroshi@gmail.com>
2023-02-09 22:21:04 +0300

    Rename

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int nachalnSchet = 100;
-        int popolnenie = 600;
+        int initialScore = 100;
+        int replenishment = 600;
         int bonus;
-        if (popolnenie >= 1000) {
-            bonus = popolnenie / 100;
-        } else bonus = 0;
-        int itogSchet = nachalnSchet + popolnenie + bonus;
+        if (replenishment >= 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        int finalScore = initialScore + replenishment + bonus;
 
-        System.out.println("Итого на счете: " + itogSchet);
+        System.out.println("Итого на счете: " + finalScore);
         System.out.println("Начислено бонусов: " + bonus);
     }
 }
